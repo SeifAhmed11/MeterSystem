38650f9a3751f70c010f431aaef6013c0f8b4611
mohamednoaman705 <m.noaman705@gmail.com>
2025-07-28 16:48:06 +0300

    Generic Repo

---
+++ MeterSystem.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace MeterSystem.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetOneAsync(Expression<Func<T,bool>> filter , bool isTracking = false , string? props = null);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter, bool isTracking = false, string? props = null);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ MeterSystem.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,68 @@
+﻿using MeterSystem.Domain.Interfaces;
+using MeterSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MeterSystem.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly MeterSystemDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(MeterSystemDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter, bool isTracking = false, string? props = null)
+        {
+            if (filter == null) 
+            {
+                return await _dbSet.ToListAsync();
+            }
+            var Data = _dbSet.Where(filter);
+            if (isTracking)
+                Data = Data.AsNoTracking();
+            if (props is not null)
+            {
+                foreach (var item in props.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Data = Data.Include(item.Trim());
+                }
+            }
+            return await Data.ToListAsync();
+        }
+
+        public async Task<T?> GetOneAsync(Expression<Func<T, bool>> filter , bool isTracking = false, string? props = null)
+        {
+            var Data = _dbSet.Where(filter);
+            if (isTracking)
+                Data = Data.AsNoTracking();
+            if(props is not null)
+            {
+                foreach (var item in props.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Data = Data.Include(item.Trim());
+                }
+            }
+            return await Data.FirstOrDefaultAsync();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);   
+            return Task.CompletedTask;
+        }
+    }
+}
