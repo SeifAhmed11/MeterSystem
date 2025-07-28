cefe72d964270f8563cefe855e6125cd7e05c9f7
mohamednoaman705 <m.noaman705@gmail.com>
2025-07-28 11:58:42 +0300

    update Entities

--- MeterSystem.Domain/Entities/Meter.cs
+++ MeterSystem.Domain/Entities/Meter.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MeterSystem.Domain.Entities
+﻿namespace MeterSystem.Domain.Entities
 {
     public class Meter
     {
         public Guid Id { get; set; }
-
         public string Serial { get; set; }
-
         public string Type { get; set; }
-
         public DateTime InstalledDate { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
         public DateTime UpdatedAt { get; set; }
-
         //relation with User
     }
 }
--- MeterSystem.Domain/Entities/Recharge.cs
+++ MeterSystem.Domain/Entities/Recharge.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MeterSystem.Domain.Entities
+﻿namespace MeterSystem.Domain.Entities
 {
     public class Recharge
     {
         public Guid Id { get; set; }
-
         public decimal Amount { get; set; }
-
         public DateTime RechargeDate { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
         //relation with User 
-
         //relation with meter
-
         //relation with customer
     }
 }
