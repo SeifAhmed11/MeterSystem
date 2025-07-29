ec634b88a5c4b93204e9dcc2b0f7dae63d0c1572
mohamednoaman705 <m.noaman705@gmail.com>
2025-07-29 11:32:09 +0300

    update IsTraking

--- MeterSystem.Domain/Interfaces/IGenericRepository.cs
+++ MeterSystem.Domain/Interfaces/IGenericRepository.cs
@@ -4,8 +4,8 @@ namespace MeterSystem.Domain.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T?> GetOneAsync(Expression<Func<T,bool>> filter , bool isTracking = false , string? props = null);
-        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter, bool isTracking = false, string? props = null);
+        Task<T?> GetOneAsync(Expression<Func<T,bool>>? filter = null, bool isTracking = false , string? props = null);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool isTracking = false, string? props = null);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
