d62eb58f1064136b7b636eab9e0b09e79134a434
sefo <1seif1ahmed1@gmail.com>
2025-07-29 10:57:22 +0300

    make RechargeMapping

--- MeterSystem.Core/Mapping/RechargeMapping.cs
+++ MeterSystem.Core/Mapping/RechargeMapping.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MeterSystem.Core.DTOs.Recharge;
+using MeterSystem.Domain.Entities;
 
 namespace MeterSystem.Core.Mapping
 {
-    internal class RechargeMapping
+    public static class RechargeMapping
     {
+        public static RechargeDto ToDto(this Recharge recharge)
+        {
+            return new RechargeDto
+            {
+                Id = recharge.Id,
+                Amount = recharge.Amount,
+                RechargeDate = recharge.RechargeDate,
+                MeterId = recharge.MeterId,
+                CustomerId = recharge.CustomerId,
+                CreatedAt = recharge.CreatedAt,
+                UpdatedAt = recharge.UpdatedAt
+            };
+        }
+
+        public static Recharge ToEntity(this CreateRechargeDto dto)
+        {
+            return new Recharge
+            {
+                Id = Guid.NewGuid(),
+                Amount = dto.Amount,
+                RechargeDate = dto.RechargeDate,
+                MeterId = dto.MeterId,
+                CustomerId = dto.CustomerId,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+        }
+
+        public static void MapToEntity(this UpdateRechargeDto dto, Recharge recharge)
+        {
+            recharge.Amount = dto.Amount;
+            recharge.RechargeDate = dto.RechargeDate;
+            recharge.MeterId = dto.MeterId;
+            recharge.CustomerId = dto.CustomerId;
+            recharge.UpdatedAt = DateTime.UtcNow;
+        }
     }
 }
