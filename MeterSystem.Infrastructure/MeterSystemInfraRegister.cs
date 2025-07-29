9558d675be1cec48dce2ee351bc4c5a33d178d19
sefo <1seif1ahmed1@gmail.com>
2025-07-29 11:55:37 +0300

    edit register

--- MeterSystem.Infrastructure/MeterSystemInfraRegister.cs
+++ MeterSystem.Infrastructure/MeterSystemInfraRegister.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MeterSystem.Domain.Interfaces;
+using MeterSystem.Infrastructure.Data;
 using MeterSystem.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MeterSystem.Infrastructure
 {
-    public static class MeterSystenInfraRegister
+    public static class MeterSystemInfraRegister
     {
         public static void Register(this IServiceCollection services) 
         { 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
 }
