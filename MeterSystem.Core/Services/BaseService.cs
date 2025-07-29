35c1d8763440c48974d52b74705948cf977ea170
mohamednoaman705 <m.noaman705@gmail.com>
2025-07-29 11:48:38 +0300

    Base service

---
+++ MeterSystem.Core/Services/BaseService.cs
@@ -0,0 +1,16 @@
+﻿using MeterSystem.Core.Responses;
+
+namespace MeterSystem.Core.Services
+{
+    public abstract class BaseService
+    {
+        protected Guid GenerateId() => Guid.NewGuid();
+        protected DateTime GetCurrentTime() => DateTime.UtcNow;
+
+        protected BaseResponse<T> Success<T>(T data, string message = "") =>
+            BaseResponse<T>.SuccessResult(data, message);
+
+        protected BaseResponse<T> Fail<T>(string message) =>
+            BaseResponse<T>.FailResult(message);
+    }
+}
---
+++ MeterSystem.Infrastructure/MeterSystemInfraRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeterSystem.Domain.Interfaces;
+using MeterSystem.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeterSystem.Infrastructure
+{
+    public static class MeterSystenInfraRegister
+    {
+        public static void Register(this IServiceCollection services) 
+        { 
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
