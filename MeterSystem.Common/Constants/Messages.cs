94fbed25b40585b59e0622935d35e953c68aa7ae
sefo <1seif1ahmed1@gmail.com>
2025-07-30 12:34:57 +0300

    update static class

--- MeterSystem.Common/Constants/Messages.cs
+++ MeterSystem.Common/Constants/Messages.cs
@@ -3,17 +3,15 @@
     public static class Messages
     {
         // SuccessMessages
-        public static string Created(string entityName) => $"{entityName} created successfully.";
-        public static string Updated(string entityName) => $"{entityName} updated successfully.";
-        public static string Deleted(string entityName) => $"{entityName} deleted successfully.";
-        public static string Loaded(string entityName) => $"{entityName} loaded successfully.";
+        public static string Created = "created successfully";
+        public static string Updated = "updated successfully.";
+        public static string Deleted= "deleted successfully.";
+        public static string Loaded= "loaded successfully.";
 
         // ErrorMessages
-        public static string NotFound(string entityName) => $"{entityName} not found.";
-        public static string Required(string fieldName) => $"{fieldName} is required.";
-        public static string Invalid(string fieldName) => $"{fieldName} is invalid.";
-        public static string AlreadyExists(string entityName) => $"{entityName} already exists.";
-        public static string MustBeGreaterThan(string field, decimal value) => $"{field} must be greater than {value}.";
-        public static string MustBeGreaterOrEqual(string field1, string field2) => $"{field1} must be greater than or equal to {field2}.";
+        public static string NotFound=  "not found.";
+        public static string Required = "is required.";
+        public static string Invalid = "is invalid.";
+        public static string AlreadyExists =  "already exists.";
     }
 }
